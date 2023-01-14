b3c03e627cd1db2d603b2eade132bcbac35d40e7
lucas.correa <contato.lucasecorrea@gmail.com>
2023-01-14 15:41:01 -0300

    Ajustes de compilação e criação do menu

--- src/screens/Main/AdList/index.tsx
+++ src/screens/Main/AdList/index.tsx
@@ -1,50 +1,67 @@
 import {useNavigation} from '@react-navigation/native';
-import React, { Fragment, useState } from 'react';
+import React, {Fragment, useState} from 'react';
 import {SafeAreaView, Text, TextInput, TouchableOpacity, View, Image} from 'react-native';
 import tw from '../../../../service/tw';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {AddListMock} from '../../../../src/Mocks/AdListMock';
-import { ScrollView } from 'react-native-gesture-handler';
+import {ScrollView} from 'react-native-gesture-handler';
+import Menu from '../../../components/Menu';
 
-const  AddList = () => {
-  const navigation = useNavigation();
-  const insets = useSafeAreaInsets();
+const AddList = () => {
+	const navigation = useNavigation();
+	const insets = useSafeAreaInsets();
 
-    return (
-     <View style={tw`flex-1`}>
-        <View style={tw`ios:h-${Math.floor(insets.top / 4)}`}>
-          <SafeAreaView style={tw`bg-red-700`} />
-        </View>
-        <SafeAreaView style={tw`flex-1 bg-red-700`}>
-          <ScrollView style={tw`flex-1`}>
-            <View style={tw`flex-row pl-5 top-15 items-center`}>
-              <TouchableOpacity style={tw`rounded-lg h-11 justify-center items-center bg-red-700`}
-                  onPress={() => navigation.navigate('Login')}>
-                <Text style={tw`text-white`}>Voltar</Text>
-              </TouchableOpacity>
-              <Text style={tw`text-white text-sm pl-5`}>Pesquisa</Text>
-              <TouchableOpacity onPress={() => navigation.navigate('CadastroAnuncio')}><Text>Anunciar</Text></TouchableOpacity>
-            </View>
-              <View style={tw`flex-1 px-5 justify-center bg-white mt-20`}>            
-                  {AddListMock.map((item, idx) => (
-                   <View style={tw`mt-5 rounded border border-slate-400 `}> 
-                      <Image style={tw`h-40 w-[100%]`} source={item.url} /> 
-                      <View style={tw`m-4 mt-2`}>
-                        <Text style={tw`text-xl`}>{item.profissao}</Text>
-                        <Text style={tw`text-xs pt-2`}>{item.descricao}</Text>
-                        <Text style={tw`text-lg pt-2`}>R$ {item.preco}</Text>
-                        <View style={tw`flex-row justify-between pt-2`}>
-                          <Text>{item.cidade} - {item.Estado}</Text>
-                          <Text>{item.nome}</Text>
-                        </View>
-                      </View>
-                   </View>
-                  ))}
-              </View>
-              </ScrollView>
-        </SafeAreaView>
-      </View>
-    )
-  }
+	return (
+		<View style={tw`flex-1`}>
+			<View style={tw`ios:h-${Math.floor(insets.top / 4)}`}>
+				<SafeAreaView style={tw`bg-red-700`} />
+			</View>
+			<SafeAreaView style={tw`flex-1 bg-red-700`}>
+				<ScrollView style={tw`flex-1`}>
+					<View style={tw`flex-row pl-[75%] top-15 items-center`}>
+						<TouchableOpacity style={tw`flex-row justify-center items-center bg-red-700`}
+							onPress={() => {
+								navigation.navigate('Login');
+							}}>
+							<Text style={tw`text-white`}>Logout</Text>
+							<Image style={tw`rounded h-8 w-8`} source = {require('D:/Projetos/app-bico/src/images/logout.png')} />
+						</TouchableOpacity>
+					</View>
+					<View style={tw`flex-1 px-5 justify-center bg-gray-50 mt-20 pb-2`}>
+						<View style={tw`flex-1 px-5 pt-4 justify-center`}>
+							<View style={tw`flex-row items-center h-10 `}>
+								<TouchableOpacity style={tw`flex-1 flex-row rounded-lg h-11 justify-center items-center`}
+									onPress={() => null}>
+									<Image style={tw`rounded h-5 w-5`} source = {require('D:/Projetos/app-bico/src/images/pesquisar.png')} />
+									<Text style={tw`pl-3`}>Pesquisar</Text>
+								</TouchableOpacity>
+								<TouchableOpacity style={tw`flex-1 flex-row rounded-lg h-11 justify-center items-center`}
+									onPress={() => null}>
+									<Image style={tw`rounded h-5 w-5`} source = {require('D:/Projetos/app-bico/src/images/ordenar.png')} />
+									<Text style={tw`pl-3`}>Ordenar</Text>
+								</TouchableOpacity>
+							</View>
+						</View>
+						{AddListMock.map(item => (
+							<View style={tw`mt-5 rounded border border-slate-400`} key={item.id}>
+								<Image style={tw`h-60 w-[100%]`} source={item.url} />
+								<View style={tw`m-4 mt-2`}>
+									<Text style={tw`text-xl`}>{item.profissao}</Text>
+									<Text style={tw`text-sm pt-2`}>{item.descricao}</Text>
+									<Text style={tw`text-lg pt-2`}>R$ {item.preco}</Text>
+									<View style={tw`flex-row justify-between pt-2`}>
+										<Text>{item.cidade} - {item.Estado}</Text>
+										<Text>{item.nome}</Text>
+									</View>
+								</View>
+							</View>
+						))}
+					</View>
+				</ScrollView>
+				<Menu tw={tw} />
+			</SafeAreaView>
+		</View>
+	);
+};
 
 export default AddList;
